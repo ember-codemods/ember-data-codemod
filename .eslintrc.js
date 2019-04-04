578ac987cc64e1702185c3edd13068a46fd08b65
Cyrille David <cyrille@qonto.eu>
2019-04-04 17:59:27 +0200

    Init the repo

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended', 'plugin:node/recommended', 'prettier'],
+  plugins: ['prettier', 'node'],
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  env: {
+    node: true,
+  },
+  rules: {
+    'prettier/prettier': ['error', {singleQuote: true}],
+  },
+  overrides: [
+    {
+      files: ['__tests__/*.test.js'],
+      plugins: ['jest'],
+
+      // can't use `extends` in nested config :sob:
+      rules: require('eslint-plugin-jest').configs.recommended.rules,
+
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
