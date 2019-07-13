38dd4931a06f57badf801d2346659a68bd25dbb7
Cyrille David <dcyriller@gmail.com>
2019-07-13 13:15:54 +0200

    Update the codemod

--- transforms/globals-to-ember-data-imports.js
+++ transforms/globals-to-ember-data-imports.js
@@ -112,12 +112,10 @@ function transform(file, api /*, options*/) {
     let mappings = {};
 
     for (let mapping of MAPPINGS) {
-      if (!mapping.deprecated) {
-        mappings[mapping.global.substr('DS.'.length)] = new Mapping(
-          mapping,
-          registry
-        );
-      }
+      mappings[mapping.global.substr('DS.'.length)] = new Mapping(
+        mapping,
+        registry
+      );
     }
 
     return mappings;
@@ -673,8 +671,8 @@ class Replacement {
 
 class Mapping {
   constructor(options, registry) {
-    this.source = options.module;
-    this.imported = options.export;
+    this.source = options.replacement.module;
+    this.imported = options.replacement.export;
     this.local = options.localName;
     this.registry = registry;
   }
