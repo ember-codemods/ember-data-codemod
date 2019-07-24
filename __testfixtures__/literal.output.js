6341cf19d613d10fec93ad107b8d09f39390ef98
snewcomer <snewcomer24@gmail.com>
2019-07-23 22:40:15 -0700

    remove space;

--- __testfixtures__/literal.output.js
+++ __testfixtures__/literal.output.js
@@ -3,4 +3,3 @@ import { hasMany, belongsTo } from '@ember-data/model';
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import { InvalidError, ServerError, TimeoutError, NotFoundError  } from '@ember-data/adapter/error';
 import Transform from '@ember-data/serializer/transform';
-
