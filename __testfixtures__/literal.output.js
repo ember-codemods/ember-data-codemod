5ee072918bf70ec6af6d76059f5c91fb84168416
snewcomer <snewcomer24@gmail.com>
2019-07-24 10:38:21 -0700

    actually...

--- __testfixtures__/literal.output.js
+++ __testfixtures__/literal.output.js
@@ -1,5 +1,4 @@
-import Model, { attr } from '@ember-data/model';
-import { hasMany, belongsTo } from '@ember-data/model';
+import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import { InvalidError, ServerError, TimeoutError, NotFoundError  } from '@ember-data/adapter/error';
 import Transform from '@ember-data/serializer/transform';
