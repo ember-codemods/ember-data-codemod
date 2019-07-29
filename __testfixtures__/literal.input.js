6313ddb794f41002dbf0af2d8b5667bfc4efd57c
snewcomer <snewcomer24@gmail.com>
2019-07-29 06:45:11 -0700

    update to correct import

--- __testfixtures__/literal.input.js
+++ __testfixtures__/literal.input.js
@@ -1,4 +1,5 @@
-import Model, { attr } from 'ember-data/model';
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
 import { hasMany, belongsTo } from 'ember-data/relationships';
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import { InvalidError, ServerError, TimeoutError, NotFoundError  } from 'ember-data/adapter/error';
